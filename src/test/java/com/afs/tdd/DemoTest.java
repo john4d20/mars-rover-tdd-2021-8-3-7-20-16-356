9f887b5d75d33bafcf4297002a87333ec3a7b9a8
OOCLDM\LEEJO <john.lee@oocl.com>
2021-12-02 16:25:54 +0800

    2 should_locationX_add_one_space_when_execute_command_given_direction_N_and_command_M()

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DemoTest {
     @Test
-    void should_locationY_add_one_space_when_execute_command_given_direction_N_and_command_N() {
+    void should_locationY_add_one_space_when_execute_command_given_direction_N_and_command_M() {
 //        given
         MarsRover marsRover = new MarsRover(0,0,"N");
         String command = "M";
@@ -17,6 +17,21 @@ class DemoTest {
 
 //        then
         assertEquals("0 1 N",result);
+
+    }
+
+    @Test
+    void should_locationX_add_one_space_when_execute_command_given_direction_N_and_command_M() {
+//        given
+        MarsRover marsRover = new MarsRover(0,0,"N");
+        String command = "M";
+
+//        when
+        marsRover.executeCommand(command);
+        String result = marsRover.getStatus();
+
+//        then
+        assertEquals("1 0 N",result);
 
     }
 }
